67ed6d5c6869f76a0846b80c07b3be031215d64f
Grégory Alvim <gregori.jagua@gmail.com>
2024-01-31 20:44:41 -0300

    created tests e2e to app

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Seu e-email').fill('gregori.jagua@gmail.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText(
+    'Enviamos um link de autenticação para seu e-mail',
+  )
+
+  await expect(toast).toBeVisible()
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Seu e-email').fill('wrong.jagua@gmail.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Credenciais inválidas')
+
+  await expect(toast).toBeVisible()
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Novo estabelecimento' })
+
+  expect(page.url()).toContain('/sign-up')
+})
