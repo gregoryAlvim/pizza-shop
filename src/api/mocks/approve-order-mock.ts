cdd817c9af737122e3c97a9206136a7b3bb436a5
Grégory Alvim <gregori.jagua@gmail.com>
2024-01-31 20:45:35 -0300

    created mocks methods api

---
+++ src/api/mocks/approve-order-mock.ts
@@ -0,0 +1,14 @@
+import { http, HttpResponse } from 'msw'
+
+import { AproveOrderParams } from '../aprove-order'
+
+export const approveOrderMock = http.patch<AproveOrderParams, never, never>(
+  '/orders/:orderId/approve',
+  async ({ params }) => {
+    if (params.orderId === 'error-order-id') {
+      return new HttpResponse(null, { status: 400 })
+    }
+
+    return new HttpResponse(null, { status: 204 })
+  },
+)
